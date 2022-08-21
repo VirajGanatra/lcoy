a8d2001036aa44ee24d2dd52a234fb6f63269dd9
VirajGanatra <54398386+VirajGanatra@users.noreply.github.com>
2022-08-21 16:49:10 +0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://virajganatra.github.io/',
+  site: 'https://lcoy.ae',
 
   sitemap: true,
   legacy: {
